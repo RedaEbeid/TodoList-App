6857778feec8f0615d547922f2d3f66de08c7e98
RedaEbeid <151432120+RedaEbeid@users.noreply.github.com>
2023-12-15 17:39:39 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import './App.css'
-import TodoList from './Components/TodoList';
+import './App.css' // Css Files
+import TodoList from './Components/TodoList'; // TodoList Component
 import { createTheme, ThemeProvider} from "@mui/material/styles";
-import { TodosContext } from './Context/TodosContext';  // TodosContext
-import { useState } from 'react'; // useState hook
-import { v4 as uuidv4 } from "uuid";  // UUid Library
+import { ToastProvider } from "./Context/ToastContext"; // ToastContext
+import TodosContextProvider from "./Context/TodosContext.js"; // TodosContextProvider
 
 const theme = createTheme({
   typography: {  fontFamily: ["Alex"]  },
   palette: {  primary: {main: "#ba000d"}  }
 });
 
-const initialTodos = [{id: uuidv4(), title: "عنوان المهمه", Details: "تفاصيل المهمه", isCompleted: false}];
-
 function App () {
-  const [todos, setTodos] = useState(initialTodos);
   
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
-        
-        <TodosContext.Provider value={{todos, setTodos}}>
-          <TodoList />
-        </TodosContext.Provider>
 
-      </div>
+      <TodosContextProvider>
+
+        <ToastProvider>
+          <div className="App">
+              <TodoList />
+          </div>
+        </ToastProvider>
+
+      </TodosContextProvider>
+
     </ThemeProvider>
   );
 }
--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -6,98 +6,22 @@ import IconButton from "@mui/material/IconButton"; // Icon
 import CheckIcon from "@mui/icons-material/Check"; // Icon
 import EditIcon from "@mui/icons-material/Edit"; // Icon
 import DeleteIcon from "@mui/icons-material/Delete"; // Icon
-import { useContext } from "react"; // useContext hook
-import { TodosContext } from "../Context/TodosContext"; // useContext hook
-import Button from "@mui/material/Button"; // Dialog
-import Dialog from "@mui/material/Dialog"; // Dialog
-import DialogActions from "@mui/material/DialogActions"; // Dialog
-import DialogContent from "@mui/material/DialogContent"; // Dialog
-import DialogContentText from "@mui/material/DialogContentText"; // Dialog
-import DialogTitle from "@mui/material/DialogTitle"; // Dialog
-import { useState } from 'react'; // useState hook
-import TextField from "@mui/material/TextField"; // Update Dialog
+import { useToast } from '../Context/ToastContext';
+import { useTodos } from "../Context/TodosContext"; // useContext hook
 
-export default function Todo({todo}) {
+export default function Todo({todo, deleteDialog, UpdateDialog}) {
+    const { todos, dispatch } = useTodos();
     
-    const { todos, setTodos } = useContext(TodosContext);
-    
+    const { showHideToast } = useToast();
+
     function handleCheckClick () {
-        let updatedTodos = todos.map((t) => {
-            if (t.id === todo.id) {t.isCompleted = !t.isCompleted}
-            return t;
-        });
-            
-        setTodos(updatedTodos);
-        localStorage.setItem("Todos", JSON.stringify(updatedTodos));
-    }
-
-
-    // Delete Dialog Logic
-    const [showDeleteDialog, setShowDeleteDialog] = useState(false);
-
-    function handleDeleteClick () { setShowDeleteDialog(true); }
-
-    function handleDeleteDialogClose () { setShowDeleteDialog(false); }
-    
-    function handleDeleteConfirm () {
-        let newTodo = todos.filter((t) => t.id !== todo.id);
-        setTodos(newTodo);
-        localStorage.setItem("Todos", JSON.stringify(newTodo));
-        setShowDeleteDialog(false);
-    }
-    
-
-    // Update Dialog Logic
-    const [showUpdateDialog, setShowUpdateDialog] = useState(false);
-    const [UpdateTodo, setUpdateTodo] = useState({title: todo.title, Details: todo.Details});
-    
-    function handleUpdateClick() { setShowUpdateDialog(true); }
-
-    function handleUpdateClose() { setShowUpdateDialog(false); }
-
-    function handleUpdateConfirm() {
-        let updatedTodos = todos.map((t) => {
-            if (t.id === todo.id) {
-                return {...t, title: UpdateTodo.title, Details: UpdateTodo.Details}
-            } else { return t; }
-        });
-        setTodos(updatedTodos);
-        localStorage.setItem("Todos", JSON.stringify(updatedTodos));
-        setShowUpdateDialog(false);
+        dispatch( { type: 'completed',payload: {id: todo.id} } )
+        showHideToast('تمت الإضافه إلي المهام المنجزه');
     }
 
 
     return (
         <>
-        {/* __ Delete Dialog __ */}
-        <Dialog open={showDeleteDialog} onClose={handleDeleteDialogClose} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
-            <DialogTitle id="alert-dialog-title">هل أنت متأكد من حذف المهمه ؟ </DialogTitle>
-            
-            <DialogContent>
-                <DialogContentText id="alert-dialog-description" style={{direction: 'rtl'}}>لا يمكنك التراجع عن الحذف بعد إتمامه</DialogContentText>
-            </DialogContent>
-
-            <DialogActions style={{display: 'flex', justifyContent: 'space-between'}}>
-                <Button onClick={handleDeleteDialogClose} > إلغاء </Button>
-                <Button onClick={handleDeleteConfirm} > تأكيد الحذف </Button>
-            </DialogActions>
-        </Dialog>
-        
-        {/* __ Update Dialog __ */}
-        <Dialog open={showUpdateDialog} onClose={handleUpdateClose} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
-            <DialogTitle id="alert-dialog-title" style={{textAlign: 'center'}} > تعديل المهمه </DialogTitle>
-            
-            <DialogContent>
-                <TextField value={UpdateTodo.title} onChange={(e)=> setUpdateTodo({...UpdateTodo, title: e.target.value})}  label="عنوان المهمه" type="text" fullWidth variant="standard" style={{direction: 'rtl'}} autoFocus margin="dense" id="name"  />
-                <TextField value={UpdateTodo.Details} onChange={(e) => setUpdateTodo({...UpdateTodo, Details: e.target.value})}  label="التفاصيل" type="text" fullWidth variant="standard" style={{direction: 'rtl'}} autoFocus margin="dense" id="name"  />
-            </DialogContent>
-
-            <DialogActions style={{display: 'flex', justifyContent: 'space-between'}}>
-                <Button onClick={handleUpdateClose} > إلغاء </Button>
-                <Button onClick={handleUpdateConfirm} > تأكيد </Button>
-            </DialogActions>
-        </Dialog>
-
             {/* __ Card __ */}
         <Card sx={{ minWidth: 275}} className='todoCard'>
             <CardContent>
@@ -114,12 +38,12 @@ export default function Todo({todo}) {
                         </IconButton>
 
                         {/* Edit Icon Button */}
-                        <IconButton value={showUpdateDialog}  onClick={ () => handleUpdateClick() } aria-label="edite" className='iconButton' style={{background: '#fff', color: '#1769aa', border: '3px solid #1769aa'}}>
+                        <IconButton value={UpdateDialog}  onClick={ () => UpdateDialog(todo) } aria-label="edite" className='iconButton' style={{background: '#fff', color: '#1769aa', border: '3px solid #1769aa'}}>
                             <EditIcon />
                         </IconButton>
 
                         {/* Delete Icon Button */}
-                        <IconButton value={showDeleteDialog} onClick={ () => handleDeleteClick() } aria-label="delete" className='iconButton' style={{background: '#fff', color: '#b23c17', border: '3px solid #b23c17'}}>
+                        <IconButton value={deleteDialog} onClick={ () => deleteDialog(todo) } aria-label="delete" className='iconButton' style={{background: '#fff', color: '#b23c17', border: '3px solid #b23c17'}}>
                             <DeleteIcon />
                         </IconButton>
                         
